67449d4b3dff38e31b9dc8c3fcf6035cc8d77c32
pipash <pipash@pipash.com>
2021-02-08 08:25:31 +0600

    first commit

---
+++ src/main/java/com/test/demo/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.test.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "another greetings from hello controller!";
+    }
+}
